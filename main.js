833e208ff9a6eb8f9fe0e209e31e5ff4830dcc27
Cody Pearce <codyapearce@gmail.com>
2017-01-01 21:06:43 -0700

    removed crash-reporter, fixed close and minize buttons

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
-require('crash-reporter').start();
 
 var mainWindow = null;
 
